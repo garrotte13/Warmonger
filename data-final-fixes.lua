eeec8f58f79fd24a181243073656b5544041fb9a
Alexander <95149904+garrotte13@users.noreply.github.com>
2021-12-06 23:38:42 +0300

    RA

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -11,9 +11,18 @@ end
 table.insert(data.raw.technology["military-science-pack"].effects, { type = "unlock-recipe", recipe = "biters-research-data"})
 table.insert(data.raw.recipe["poison-capsule"].ingredients, {type="item", name="biomass", amount=1})
 
--- RampantArsenal
--- table.insert( data.raw.recipe["repair"].normal.ingredients, {type="item", name="biomass", amount=2} )
---
+-- if mods["RampantArsenal"] then
+--   local repaircapsulerecipe = data.raw.recipe["repair"]
+--   for i, component in pairs(repaircapsulerecipe.ingredients) do
+--     for _, value in pairs(component) do
+--       if value == "stone-wall" then I'm kidding
+--         repaircapsulerecipe.ingredients[i] = {type="item", name="biters-research-data", amount=1}
+--         break
+--       end
+--     end
+--   end
+  -- table.insert( data.raw.recipe["repair"].normal.ingredients, {type="item", name="biomass", amount=2} )
+-- end
 
 if mods["IndustrialRevolution"] then
   local militaryrecipe = data.raw.recipe["military-science-pack"]
