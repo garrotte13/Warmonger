eb0a386b2e96ee4e5443be61098fa1dc2e45b523
Alexander <95149904+garrotte13@users.noreply.github.com>
2021-12-06 22:37:35 +0300

    IR2 compatibility

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -14,3 +14,27 @@ table.insert(data.raw.recipe["poison-capsule"].ingredients, {type="item", name="
 -- RampantArsenal
 -- table.insert( data.raw.recipe["repair"].normal.ingredients, {type="item", name="biomass", amount=2} )
 --
+
+if mods["IndustrialRevolution"] then
+  local militaryrecipe = data.raw.recipe["military-science-pack"]
+  for i, component in pairs(militaryrecipe.ingredients) do
+      for _, value in pairs(component) do
+        if value == "gunpowder" then
+          militaryrecipe.ingredients[i] = {type="item", name="biters-research-data", amount=1}
+          break
+        end
+      end
+  end
+  local bitersresearch = data.raw.recipe["biters-research-data"]
+  for i, component in pairs(bitersresearch.ingredients) do
+    for _, value in pairs(component) do
+      if value == "stone-wall" then
+        bitersresearch.ingredients[i] = {type="item", name="gunpowder", amount=6}
+        break
+      end
+    end
+  end
+
+  -- Bioprocessing recipe will be fixed later if anyone notices IR2 compatibility at all...
+
+end
