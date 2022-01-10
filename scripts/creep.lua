feb40f8b228ab3bf49f8d89e84b1d3d5f8d670b2
Alexander <95149904+garrotte13@users.noreply.github.com>
2022-01-11 02:32:27 +0300

    Creep generation optimizations and balancing

--- scripts/creep.lua
+++ scripts/creep.lua
@@ -47,7 +47,7 @@ function creep.on_chunk_generated(chunk_area, surface)
   end
   local entities = surface.find_entities_filtered({ type = { "unit-spawner", "turret" }, area = chunk_area, force = "enemy" })
   for _, entity in pairs(entities) do
-    generate_creep({ entity })
+    if entity.valid then generate_creep({ entity }) end
   end
 end
 
@@ -78,11 +78,19 @@ function creep.process_creep_queue()
         creep_pack.stage = 1
     elseif creep_pack.stage == 1 then
         creep_pack.creep_tiles = {}
+        local n = 1
+        local ne_coef = 2
+        local ne_prob = 19
+        if (settings.startup["rampant--newEnemies"] and settings.startup["rampant--newEnemies"].value) then
+          ne_coef = 5.1
+          ne_prob = 9.7
+        end
         for i=1,#creep_pack.tiles do
           local r = 1 -- by default it will be biomass creep
-          local actual_tile = creep_pack.surface.get_tile(creep_pack.tiles[i].position) -- the starting point we could crash replace-path-abuse with new global array
-          local actual_name = ""
-          if actual_tile then actual_name = actual_tile.name end
+          -- local actual_tile = creep_pack.surface.get_tile(creep_pack.tiles[i].position) -- the starting point we could crash replace-path-abuse with new global array
+          -- local actual_name = ""
+          -- if actual_tile then actual_name = actual_tile.name end
+          local actual_name = creep_pack.tiles[i].name
           if actual_name == "kr-creep" then -- we mustn't change fate
             r = 4
           elseif creep_pack.fake then
@@ -91,14 +99,16 @@ function creep.process_creep_queue()
             local d = misc.get_distance(creep_pack.tiles[i].position, creep_pack.position)
             if (d > 4) and ( (creep_pack.radius - d) < 4) then   -- no biomass on distal rings
               if math.random(1,10) > 6 then r = 4 else r = 3 end  -- 60% fake creep, 40% nothing
-            elseif (d > (2 + math.floor(5.4 * game.forces.enemy.evolution_factor)) ) then -- bigger and bigger 100% biomass core
-              if math.random(1,(3 + math.floor(5.5 * game.forces.enemy.evolution_factor))) > 1 then r = 3 end -- less biomass with every ~15% evo increase
+            elseif (d > (2 + math.floor(ne_coef * game.forces.enemy.evolution_factor)) ) then -- bigger and bigger 100% biomass core underneath growing New Enemies structures
+              if math.random(1,(4 + math.ceil(ne_prob * game.forces.enemy.evolution_factor))) > 1 then r = 3 end -- less biomass with every ~9% or 6% evo increase
             end
           end
             if r < 3 then
-              creep_pack.creep_tiles[i] = { name = "kr-creep", position = creep_pack.tiles[i].position }
+              creep_pack.creep_tiles[n] = { name = "kr-creep", position = creep_pack.tiles[i].position }
+              n = n + 1
             elseif (r == 3) and (actual_name ~="fk-creep" ) then -- if placing fake creep we need to avoid redundant work
-              creep_pack.creep_tiles[i] = { name = "fk-creep", position = creep_pack.tiles[i].position }
+              creep_pack.creep_tiles[n] = { name = "fk-creep", position = creep_pack.tiles[i].position }
+              n = n + 1
             end
         end
         creep_pack.stage = 2
@@ -107,6 +117,24 @@ function creep.process_creep_queue()
         creep_pack.stage = 3
     elseif creep_pack.stage == 3 then
       if global.corrosion.enabled then
+        if creep_pack.fake and creep_pack.position then
+          local entities = creep_pack.surface.find_entities_filtered{ position = creep_pack.position, radius = creep_pack.radius,  force = "player" }
+          for _, entity in pairs(entities) do
+            if entity.valid and entity.destructible and entity.is_entity_with_health then
+              local turret_area = entity.selection_box
+              area.ceil(turret_area)
+              local hitpoints = entity.max_health -- Need to add health_bonus for player here
+              local dmg = math.ceil( hitpoints * ( 0.1 + game.forces.enemy.evolution_factor/4 ) ) -- bigger one time damage and can be lethal
+              local recieved_dmg = entity.damage(dmg, "enemy", "acid")
+              game.print("Natives strike back with lethal corrosion on your: " .. entity.name .. ". Damage received: " .. recieved_dmg)
+              -- add check for remaining health here !
+              if not global.corrosion.affected[turret_area.left_top.x .. ":" .. turret_area.left_top.y] then
+                global.corrosion.affected[turret_area.left_top.x .. ":" .. turret_area.left_top.y] = entity
+                global.corrosion.affected_num = global.corrosion.affected_num + 1
+              end
+            end
+          end
+        else
         for _, tile in pairs(creep_pack.creep_tiles) do
           local entities = creep_pack.surface.find_entities_filtered{ position = tile.position, force = "player" }
           for _, entity in pairs(entities) do
@@ -114,16 +142,17 @@ function creep.process_creep_queue()
               local turret_area = entity.selection_box
               area.ceil(turret_area)
               if not global.corrosion.affected[turret_area.left_top.x .. ":" .. turret_area.left_top.y] then
-              global.corrosion.affected[turret_area.left_top.x .. ":" .. turret_area.left_top.y] = entity
-              global.corrosion.affected_num = global.corrosion.affected_num + 1
+                global.corrosion.affected[turret_area.left_top.x .. ":" .. turret_area.left_top.y] = entity
+                global.corrosion.affected_num = global.corrosion.affected_num + 1
               end
             end
           end
         end
       end
+    end
       global.creep.creep_queue[global.creep.last_creep_id_counter] = nil
       global.creep.last_creep_id_counter = global.creep.last_creep_id_counter + 1
-    end
+  end
 end
 
 creep.remote_interface = {
