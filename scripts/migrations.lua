1b4129ef03217b465d2e05e41f82207a3a4c73bc
Alexander <95149904+garrotte13@users.noreply.github.com>
2023-01-07 14:03:29 +0300

    Refactored mod ver migration code; Updated CrMiner polllution startup setting.

--- scripts/migrations.lua
+++ scripts/migrations.lua
@@ -13,7 +13,15 @@ function migrations.generic(ChangedModsData)
   creep.update()
   if ChangedModsData.mod_changes["Warmonger"] then
     local old = ChangedModsData.mod_changes["Warmonger"].old_version
-    if old and (old:match("1%.2%.%d") or old:match("1%.0%.%d") or old:match("1%.1%.%d") ) then
+
+    if not old then
+      return
+    end
+    local major_str, minor_str, build_ver_str = string.match(old, "(%d)%.(%d)%.(%d+)")
+    local major = tonumber(major_str)
+    local minor = tonumber(minor_str)
+    local build_ver = tonumber(build_ver_str)
+    if minor < 3 then
       creep_eater.init()
       if game.forces["player"].technologies["advanced-material-processing"].researched then
         game.forces["player"].recipes["creep-miner0-radar"].enabled = true
@@ -25,49 +33,27 @@ function migrations.generic(ChangedModsData)
         game.forces["player"].recipes["wm-residue-sulphuric-acid"].enabled = true
       end
     end
-    if old and (old:match("1%.0%.%d") ) then
+    if minor == 0 then
       corrosion.init()
     end
-    if old and (old:match("1%.3%.%d") ) then
+    if minor == 3 and build_ver < 8 then
       settings.global["wm-CounterStrike"] = {value = global.corrosion.strike_back}
       settings.global["wm-CreepMinerHints"] = {value = global.corrosion.creepminer_hints}
     end
-    if old and (old:match("1%.2%.%d") or old:match("1%.3%.%d")) then
+    if (minor == 2) or ( minor == 3 and build_ver < 8 ) then
       settings.global["wm-CreepCorrosion"] = {value = global.corrosion.enabled}
     end
-    if old and (old=="1.3.9" or old=="1.3.8") then
+    if minor == 3 and ( build_ver == 9 or build_ver == 8 ) then
       if settings.global["wm-CreepMiningPollution"].value ~= settings.startup["wm-CreepMiningPollution_s"].value then
         game.print("Miner pollution has been migrated to Startup mod settings. Please change it manually to your preferred value of: ".. settings.global["wm-CreepMiningPollution"].value)
       end
     end
+    if minor == 3 and (build_ver > 9 and build_ver < 17) then
+     if settings.startup["wm-CreepMiningPollution_s"].value == 1.2 then game.print("Warmonger update recommends reducing Creep Miner emissions coefficient to value = 1 in Startup Mod Settings and reload.") end
+     if settings.startup["wm-CreepMiningPollution_s"].value == 0.6 then game.print("Warmonger update recommends reducing Creep Miner emissions coefficient to value = 0.5 in Startup Mod Settings and reload.") end
+    end
     global.creep_miner_refuel = settings.global["wm-CreepMinerFueling"].value
   end
 end
-
-migrations.versions = {
-  ["1.0.1"] = function()
--- game.print("Mod Warmonger changed from 1.0.1")
-    local old_global = global
-    global = {}
-
-    -- REINITIALIZE
-
-    on_tick_n.init()
-
-    creep.init()
-	  corrosion.init()
-	
-	global.corrosion.enabled = true
-
-    -- Creep
-    global.creep.on_biter_base_built = old_global.creep_on_biter_base_built
-    global.creep.on_chunk_generated = old_global.creep_on_chunk_generated
-    if not old_global.creep_on_chunk_generated then
-      global.creep.surfaces[game.get_surface("nauvis").index] = nil
-    end
-  end
-
-
-}
 
 return migrations
